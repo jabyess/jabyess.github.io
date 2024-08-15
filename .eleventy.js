7da0328be78ff5a53b93d59aab49433ceb051672
Jimmy Byess <jimmy@skelios.com>
2024-08-15 18:56:30 -0400

    add favicon, its very important

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("index.js");
     eleventyConfig.addPassthroughCopy("./fonts");
+    eleventyConfig.addPassthroughCopy("./assets");
 
     return {
         dir: {
