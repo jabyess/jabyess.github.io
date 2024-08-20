0c8720163c93ef157cdc4c81469b7af8535360c2
Jimmy Byess <jimmy@skelios.com>
2024-08-20 17:17:15 -0400

    cleanup, add index page back

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./content/index.css");
-    eleventyConfig.addPassthroughCopy("./content/index.js");
+    eleventyConfig.addPassthroughCopy("./index.css");
+    eleventyConfig.addPassthroughCopy("./index.js");
     eleventyConfig.addPassthroughCopy("./fonts");
     eleventyConfig.addPassthroughCopy("./assets");
 
     return {
-        dir: {
-            output: "docs",
-            input: "content",
-            includes: '../_includes'
-        }
+        // dir: {
+        //     // output: "docs",
+        //     // input: "content",
+        //     // includes: '../_includes'
+        // }
     }
 }
